bc0b585e8a93e42bdef4292da7d52ad60127e6ee
endalk <eb808826@gmail.com>
2021-03-24 18:57:48 +0300

    :bulb: added new layout and the functionality to monitor battery state

--- lib/screen/home/home.dart
+++ lib/screen/home/home.dart
@@ -1,87 +1,179 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:battery/battery.dart';
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
 
 class HomeScreen extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return Scaffold(
-            body: Body(),
+        body: Body(),
         );
     }
 }
-
 
 class Body extends StatelessWidget {
-
     @override
     Widget build(BuildContext context) {
-        Size size = MediaQuery.of(context).size;
+        // Size size = MediaQuery.of(context).size;
 
-        return Background(
-            child: SingleChildScrollView(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                        SvgPicture.asset(
-                            "assets/icons/battery.svg",
-                            color: Colors.green,
-                            height: size.height * 0.1,
-                        ),
-                        Text(
-                            "CHARGING MONITOR",
-                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28.0),
-                        ),
-                        SizedBox(height: size.height * 0.009),
-                        Container(
-                            padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 0.0),
-                            child: Text(
-                                "lorem ipsum this is it which can diversify. lorem ipsum this is it which can diversify lorem ipsum this is it which can diversify",
-                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
-                            ),
-                        )
-                    ],
-                ),
-            ),
-        );
+        return Background();
     }
 }
 
-class Background extends StatelessWidget {
-    final Widget child;
-    const Background({
-        Key? key,
-        required this.child,
-    }) : super(key: key);
+class Background extends StatefulWidget {
+    @override
+    _BackgroundState createState() => _BackgroundState();
+}
+
+class _BackgroundState extends State<Background> {
+
+    final Battery _battery = Battery();
+
+    String? _batteryState;
+    int? _batteryLevel;
+
+    void _getBatteryLevel() async {
+        var level;
+
+        level = await _battery.batteryLevel;
+
+        this.setState(() {
+            _batteryLevel = level;
+        });
+
+        _battery.onBatteryStateChanged.listen((BatteryState state) async {
+            level = await _battery.batteryLevel;
+            this.setState(() {
+                _batteryLevel = level;
+                _batteryState = _parseBatteryState(state);
+            });
+            // if (level >= 80) {
+            //     // FlutterRingtonePlayer.playNotification();
+            //     FlutterRingtonePlayer.play(
+            //         android: AndroidSounds.notification,
+            //         ios: IosSounds.glass,
+            //         looping: false, // Android only - API >= 28
+            //         volume: 5, // Android only - API >= 28
+            //         asAlarm: false, // Android only - all APIs
+            //     );
+            // } else {
+            //     FlutterRingtonePlayer.stop();
+            // }
+        });
+    }
 
     @override
     Widget build(BuildContext context) {
         Size size = MediaQuery.of(context).size;
-
-        return Container(
-            height: size.height,
-            width: double.infinity,
-            child: Stack(
+        _getBatteryLevel();
+        return Scaffold(
+            appBar: AppBar(
+            leading: Icon(Icons.menu),
+            title: Text('Page title'),
+            actions: [
+                Padding(padding: EdgeInsets.all(0.5), child: Icon(Icons.more_vert),)
+            ],
+            backgroundColor: Color.fromRGBO(2, 128, 144, 0), // 0, 18, 20, 0
+        ),
+        body: ListView(
+            children: [
+            Container(
+                height: size.height,
+                width: double.infinity,
+                child: Stack(
                 alignment: Alignment.center,
                 children: <Widget>[
                     Positioned(
-                        top: 0,
-                        left: 0,
-                        child: Image.asset(
-                            "assets/images/main_top.png",
-                            width: size.width * 0.3,
-                        ),
+                    top: 0,
+                    left: 0,
+                    child: Image.asset(
+                        "assets/images/main_top.png",
+                        width: size.width * 0.3,
+                    ),
                     ),
                     Positioned(
-                        bottom: 0,
-                        left: 0,
-                        child: Image.asset(
-                            "assets/images/main_bottom.png",
-                            width: size.width * 0.2,
-                        ),
+                    bottom: 0,
+                    left: 0,
+                    child: Image.asset(
+                        "assets/images/main_bottom.png",
+                        width: size.width * 0.2,
                     ),
-                    child,
+                    ),
+                    ListView(
+                    children: [
+                        Card(
+                        margin: EdgeInsets.all(20.0),
+                        color: Colors.white,
+                        elevation: 50.0,
+                        child: Container(
+                            height: 140,
+                            child: Container(
+                                alignment: Alignment.center,
+                                child: SvgPicture.asset(
+                                    "assets/icons/battery.svg",
+                                    color: Colors.green,
+                                    height: size.height * 0.1,
+                                ),
+                            )),
+                        ),
+                        Card(
+                        margin: EdgeInsets.all(40.0),
+                        color: Colors.white,
+                        elevation: 10.0,
+                        child: Container(
+                            height: 140,
+                            child: Container(
+                                alignment: Alignment.center,
+                                child: Text(
+                                _batteryState.toString() + "%",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                ),
+                                ),
+                            )),
+                        ),
+                    ],
+                    ),
                 ],
+                ),
+            ),
+            ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            selectedFontSize: 14,
+            unselectedFontSize: 14,
+            onTap: (value) {
+            // Respond to item press.
+            },
+            items: [
+                BottomNavigationBarItem(
+                    title: Text('About'),
+                    icon: Icon(Icons.favorite),
+                ),
+                BottomNavigationBarItem(
+                    title: Text('Setting'),
+                    icon: Icon(Icons.settings),
+                ),
+            ],
             ),
         );
+    }
+
+    String _parseBatteryState(BatteryState state) {
+        var stateString;
+        switch (state) {
+        case BatteryState.charging:
+            stateString = "Charging";
+            break;
+        case BatteryState.discharging:
+            stateString = "Discharging";
+            break;
+        case BatteryState.full:
+            stateString = "Fully Charged";
+            break;
+        }
+        return stateString;
     }
 }
